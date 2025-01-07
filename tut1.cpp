5bc1911fe0d358d2f23833a16a9d08a00d087304
Govind sahu <govindtonk02@gmail.com>
2025-01-07 21:48:48 +0530

    week-1
    the basic of cpp with proper syntex ,

---
+++ tut1.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    // Print a welcome message to the console
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
---
+++ tut2.cpp
@@ -0,0 +1,9 @@
+#include<iostream>>
+int main(){
+    int a=4;
+    int b=5;
+    float pi=3.14;
+    std::cout <<"WELCOME GOVIND.here the a value is "<<a<<". the value of b is "<<b;
+     std:: cout <<"\nthe value of pi is :"<<pi;
+    return 0;
+}
