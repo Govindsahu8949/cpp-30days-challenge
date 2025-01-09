7234d695fffaf9d5f8c79e4c7419e819237c06b3
Govind sahu <govindtonk02@gmail.com>
2025-01-09 21:12:15 +0530

    Create tut7.cpp
    all about if-else statements in C++

---
+++ tut7.cpp
@@ -0,0 +1,23 @@
+// Day 7 is here with ifelse statement:
+
+#include<iostream>
+using namespace std;
+
+int main(){
+int age;
+cout<<"define your age"<<endl;
+cin>>age;
+
+if (age<18){
+cout<<"you are under 18:"<<endl;
+}
+else{ 
+    cout<<"welcome you are 18"<<endl;
+
+}
+
+return 0;
+}
+
+
+
