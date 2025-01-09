450ac9aa84b1d2d752987225d4b9cf2f946bed3a
Govind sahu <govindtonk02@gmail.com>
2025-01-09 19:31:24 +0530

    Create tut6.cpp
    tutorial 6 is based on the const varible .
    which is used to be fix a varible values.
    ###like example:
    const
    pi = 3.14....

---
+++ tut6.cpp
@@ -0,0 +1,35 @@
+// const varible 
+
+
+#include<iostream>
+using namespace std;
+int main (){
+
+  
+//   int a = 34;
+//    char c = a ;
+//   cout<<"The value of a was:  "<<a<<endl;
+//   a = 45;
+//   cout<<"The value of a was: "<<a<<endl;
+// //    c ='4';
+// //   cout<<"The value of c is "<<c<<(a+c)<<endl;
+  
+
+
+// #const variable :::  its  is used  when we want to fixed a value..
+const int a=3;  //3 fix
+// cout<<"The value of a was: "<<a;
+// a= 45.6; //not show errre
+// cout<<"The value of a was: "<<a; 
+
+
+  
+  
+  
+  return 0;
+
+
+
+
+
+}
