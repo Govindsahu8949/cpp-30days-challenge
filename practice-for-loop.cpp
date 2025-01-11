24db1b1871b17cc57012e9b583e1dea7e5c3988d
Govind sahu <govindtonk02@gmail.com>
2025-01-11 21:59:54 +0530

    Add files via upload
    Signed-off-by: Govind sahu  <govindtonk02@gmail.com>

---
+++ practice-for-loop.cpp
@@ -0,0 +1,11 @@
+// #for loop question
+// print a simple loop
+// #include<iostream>
+// using namespace std;
+// int main(){
+// int i;
+// for (int i=0;i>10;i++){
+//     cout<<i<<endl;
+// }
+//     return 0;
+// }
